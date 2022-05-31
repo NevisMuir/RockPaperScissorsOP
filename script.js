40fec99fe1447ecb82c2175b6adb0b5e51ee3ee4
NevisMuir <rmuir310@gmail.com>
2022-05-31 09:16:29 -0400

    Create game buttons, working function Also put computer selection in play round, so it chooses a new option each play

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const RPS = ['rock', 'paper', 'scissors']
-        //randomly generating rock, paper or scissors
-        function computerPlay(){
-            num = Math.floor(Math.random()*3);
-            return RPS[num];
-         }
-         //the computers decision is now a variable
-         const computerSelection=computerPlay();
-         //the player is prompted with a choice, and that choice is converted to lowercase
-         
+//randomly generating rock, paper or scissors
+function computerPlay(){
+  num = Math.floor(Math.random()*3);
+  return RPS[num];
+}
 
-             //player is prompted
-         let playerChoice=prompt("Do you choose Rock, Paper, or Scissors?");
-                const playerSelection=playerChoice.toLowerCase()
-                //single round
-         function playRound(playerSelection,computerSelection){
-             if (playerSelection==computerSelection){
-                 return 'It\'s a draw! Both players chose '+playerSelection;
-             } else if(playerSelection==="rock" && computerSelection==="scissors" || playerSelection==="paper" && computerSelection==="rock" || playerSelection==="scissors" && computerSelection==="paper"){
-                 return 'You Win! '+playerSelection+' beats '+computerSelection
-             }else if(playerSelection==="rock" && computerSelection==="paper" || playerSelection==="paper" && computerSelection==="scissors" || playerSelection==="scissors" && computerSelection==="rock"){
-                 return 'You Lose! '+computerSelection+' beats '+playerSelection;
-             }
-            }
-            //computer's choice
-            console.log('Computer chose '+computerSelection)
-            //result of round
-            console.log(playRound(playerSelection,computerSelection))
+
+//single round
+function playRound(playerSelection){
+  const computerSelection=computerPlay();
+  console.log('Computer chose '+computerSelection)
+  if (playerSelection==computerSelection){
+    return 'It\'s a draw! Both players chose '+playerSelection;
+  } else if(playerSelection==="rock" && computerSelection==="scissors" || playerSelection==="paper" && computerSelection==="rock" || playerSelection==="scissors" && computerSelection==="paper"){
+    return 'You Win! '+playerSelection+' beats '+computerSelection
+  }else if(playerSelection==="rock" && computerSelection==="paper" || playerSelection==="paper" && computerSelection==="scissors" || playerSelection==="scissors" && computerSelection==="rock"){
+     return 'You Lose! '+computerSelection+' beats '+playerSelection;
+}}
+
+//button select all query variable
+const buttons = document.querySelectorAll('button');
+///button event listener, button choice should replace variable playerSelection
+buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        console.log(playRound(button.id))
+    })})
+
+
