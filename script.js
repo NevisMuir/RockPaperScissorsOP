d5c4af2390c0b2b1273d0fc9847be85806554cb4
NevisMuir <rmuir310@gmail.com>
2022-05-31 11:11:31 -0400

    Add computer result div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const RPS = ['Rock', 'Paper', 'Scissors']
 const result = document.querySelector('#result');
+const computer = document.querySelector('#computer');
 
 //randomly generating Rock, Paper or Scissors
 function computerPlay(){
@@ -11,12 +12,14 @@ function computerPlay(){
 //single round
 function playRound(playerSelection){
   const computerSelection=computerPlay();
-  console.log('Computer chose '+computerSelection)
+  computer.textContent = 'Computer chose '+computerSelection
   if (playerSelection==computerSelection){
     return 'It\'s a draw! Both players chose '+playerSelection;
-  } else if(playerSelection==="Rock" && computerSelection==="Scissors" || playerSelection==="Paper" && computerSelection==="Rock" || playerSelection==="Scissors" && computerSelection==="Paper"){
+  } else if(playerSelection==="Rock" && computerSelection==="Scissors" || playerSelection==="Paper" && computerSelection==="Rock" 
+  || playerSelection==="Scissors" && computerSelection==="Paper"){
     return 'You Win! '+playerSelection+' beats '+computerSelection
-  }else if(playerSelection==="Rock" && computerSelection==="Paper" || playerSelection==="Paper" && computerSelection==="Scissors" || playerSelection==="Scissors" && computerSelection==="Rock"){
+  }else if(playerSelection==="Rock" && computerSelection==="Paper" || playerSelection==="Paper" && computerSelection==="Scissors" 
+  || playerSelection==="Scissors" && computerSelection==="Rock"){
      return 'You Lose! '+computerSelection+' beats '+playerSelection;
 }}
 
