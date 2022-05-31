5fc56f30c1f8eee137cdb87fd135315aac228e26
NevisMuir <rmuir310@gmail.com>
2022-05-31 11:57:21 -0400

    Add score output

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const RPS = ['Rock', 'Paper', 'Scissors']
 const result = document.querySelector('#result');
 const computer = document.querySelector('#computer');
+const computerScoreBoard = document.querySelector('#computerScore')
+const playerScoreBoard = document.querySelector('#playerScore')
 
 //randomly generating Rock, Paper or Scissors
 function computerPlay(){
   num = Math.floor(Math.random()*3);
   return RPS[num];
 }
-
+let computerScore = 0
+let playerScore = 0
 
 //single round
 function playRound(playerSelection){
@@ -17,9 +20,11 @@ function playRound(playerSelection){
     return 'It\'s a draw! Both players chose '+playerSelection;
   } else if(playerSelection==="Rock" && computerSelection==="Scissors" || playerSelection==="Paper" && computerSelection==="Rock" 
   || playerSelection==="Scissors" && computerSelection==="Paper"){
+    playerScore++;
     return 'You Win! '+playerSelection+' beats '+computerSelection
   }else if(playerSelection==="Rock" && computerSelection==="Paper" || playerSelection==="Paper" && computerSelection==="Scissors" 
   || playerSelection==="Scissors" && computerSelection==="Rock"){
+    computerScore++;  
      return 'You Lose! '+computerSelection+' beats '+playerSelection;
 }}
 
@@ -29,6 +34,8 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button)=>{
     button.addEventListener('click',()=>{
         result.textContent = playRound(button.id)
+        computerScoreBoard.textContent= computerScore
+        playerScoreBoard.textContent= playerScore
     })})
 
 
